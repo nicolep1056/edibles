142ca1c08808872a7f95fea323d4a486e87d4e92
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-10-27 12:49:32 -0500

    changes

--- js/express.js
+++ js/express.js
@@ -21,7 +21,7 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "view.html"));
   });
   
-  app.get("/reserverations", function(req, res) {
+  app.get("/reservations", function(req, res) {
     res.sendFile(path.join(__dirname, "add.html"));
   });
   
